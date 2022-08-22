7b20d994281f85ace6e78df69588dc44484f4abd
Rahayu Puji Astuty <rahayupujiastuty1@gmail.com>
2022-08-22 16:49:13 +0700

    sayWhat

---
+++ hello.js
@@ -0,0 +1,15 @@
+function sayHello(name){
+    return "Hello " +name;
+}
+
+function sayGuest(){
+    return "Hello";
+}
+
+function sayNo(){
+    return "No";
+}
+
+function sayWhat(){
+    return "What?"
+}
